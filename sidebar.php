edbecd80becbed0f41096be6135cad85e535c75b
AdityaVijay1 <adityavijay1712@gmail.com>
2024-01-05 13:22:20 +0800

    Sidebar Changes

--- sidebar.php
+++ sidebar.php
@@ -10,24 +10,24 @@
     <div class="sidebar-module">
         <h4>Achievements</h4>
         <ol class="list-unstyled">
-            <li><a href="#">December 2020/a></li>
+            <li><a href="#">December 2020</li>
             <li><a href="#">July 2021</a></li>
             <li><a href="#">March 2023</a></li>
             <li><a href="#">June 2023</a></li>
             <li><a href="#">December 2023</a></li>
         </ol>
     </div>
-    <br><br><br>
+    <br><br>
     <div class="sidebar-module">
         <h4>Find Us Here</h4>
         <ol class="list-unstyled">
-            <li><a href="#">Instagram</a></li>
             <li><a href="https://www.jcu.edu.sg/openhouse">JCU OpenHouse Details</a></li>
+            <li><a href="#">Instagram</a></li>
             <li><a href="#">Twitter</a></li>
             <li><a href="#">Facebook</a></li>
         </ol>
     </div>
-
+    <br>
     <div class="sidebar-module">
         <h4>REGISTER NOW</h4>
         <ol class="list-unstyled">
